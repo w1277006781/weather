6956765889f25aca6dd7e4f96c131ef6041f407d
w1277006781 <1277006781@qq.com>
2019-09-15 21:53:06 +0800

    更改

--- src/main/java/com/utry/action/StudentAction.java
+++ src/main/java/com/utry/action/StudentAction.java
@@ -20,7 +20,7 @@ public class StudentAction extends ActionSupport {
     }
 
     public StudentEntity getStudent() {
-	System.out.println("-----------wUUuww热热热--------");
+	System.out.println("-----------wUUuww热热热万物--------");
 return student;
     }
 
